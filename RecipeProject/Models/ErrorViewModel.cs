45e3be1810f43b933e4732961dfd3698a13188c3
MaxGazley <max.gazley@gmail.com>
2026-09-22 08:24:23 -0300

    Add project files.

---
+++ RecipeProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RecipeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
